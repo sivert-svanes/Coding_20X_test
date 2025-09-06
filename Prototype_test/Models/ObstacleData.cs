4fa1e998a56e28e7e71c4d288718da829bf05a5f
sivert-svanes <siverss@uia.no>
2025-09-06 18:27:46 +0200

    endring i filene

---
+++ Prototype_test/Models/ObstacleData.cs
@@ -0,0 +1,9 @@
+﻿namespace Prototype_test.Models
+{
+    public class ObstacleData
+    {
+        public string ObstacleName { get; set; }
+        public string ObstacleHeight { get; set; }
+        public string ObstacleDescription { get; set; }
+    }
+}
